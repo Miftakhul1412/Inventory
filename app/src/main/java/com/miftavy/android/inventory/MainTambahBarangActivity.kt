7450da8516571e3b0357e99745cd9753bbfbeb6c
Nur Khofifah <fifah250801@gmail.com>
2022-03-02 13:46:09 +0700

    date picker update

--- app/src/main/java/com/miftavy/android/inventory/MainTambahBarangActivity.kt
+++ app/src/main/java/com/miftavy/android/inventory/MainTambahBarangActivity.kt
@@ -3,24 +3,39 @@ package com.miftavy.android.inventory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.miftavy.android.inventory.adapter.AdapterBarang
 import com.miftavy.android.inventory.adapter.Barang
 import com.miftavy.android.inventory.databinding.ActivityMainTambahBarangBinding
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
+import java.util.*
 
-class MainTambahBarangActivity : AppCompatActivity() {
+class MainTambahBarangActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
     private lateinit var binding : ActivityMainTambahBarangBinding
     private  lateinit var adapterBarang: AdapterBarang
+    private lateinit var calendar: Calendar
+    private lateinit var dpd: DatePickerDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainTambahBarangBinding.inflate(layoutInflater)
 
+        calendar = Calendar.getInstance()
+        dpd = DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR)
+            , calendar.get(Calendar.MONTH)
+            , calendar.get(Calendar.DAY_OF_MONTH))
+
         setContentView(binding.root)
 
         addJenisBarang()
+
+
+        binding.datePicker.setOnClickListener {
+            dpd.show(supportFragmentManager, "datePicker")
+        }
 
         //adapterBarang = AdapterBarang()
         adapterBarang = AdapterBarang{
@@ -30,6 +45,7 @@ class MainTambahBarangActivity : AppCompatActivity() {
             adapter = adapterBarang
             layoutManager = LinearLayoutManager(this@MainTambahBarangActivity)
         }
+
 
         // kasih klik untuk tombol simpannya
         binding.simpanData.setOnClickListener{
@@ -52,4 +68,11 @@ class MainTambahBarangActivity : AppCompatActivity() {
 
         binding.dropdownJenisBarang.adapter = adapter
     }
+
+    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        if(view?.tag == "datePicker"){
+            binding.datePicker.text = String.format(Locale.getDefault(), "%d-%02d-%02d", year, monthOfYear+1, dayOfMonth)
+        }
+    }
+
 }
