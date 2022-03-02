1b5ff7e412e874a18926e5f60f52637bc0349b17
Miftakhul1412 <mifta.ervy@gmail.com>
2022-03-02 10:12:14 +0700

    jenis barang

---
+++ app/src/main/java/com/miftavy/android/inventory/MainTambahJenisBarangActivity.kt
@@ -0,0 +1,11 @@
+package com.miftavy.android.inventory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainTambahJenisBarangActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_tambah_jenis_barang)
+    }
+}
