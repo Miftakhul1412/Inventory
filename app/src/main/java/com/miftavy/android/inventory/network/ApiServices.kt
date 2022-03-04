2b08b9333214822aa346dc6cda5997ff3928b83c
Nur Khofifah <fifah250801@gmail.com>
2022-03-04 16:18:43 +0700

    membuat tambah barang

--- app/src/main/java/com/miftavy/android/inventory/network/ApiServices.kt
+++ app/src/main/java/com/miftavy/android/inventory/network/ApiServices.kt
@@ -1,15 +1,12 @@
 package com.miftavy.android.inventory.network
 
-import com.miftavy.android.inventory.model.ResponseLogin
 import com.miftavy.android.inventory.input.InputLogin
-import com.miftavy.android.inventory.model.ResponseListBarang
-import com.miftavy.android.inventory.model.ResponseListJenis
-import com.miftavy.android.inventory.model.ResponseListStok
+import com.miftavy.android.inventory.model.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface ApiServices {
-    @GET("webservices/listbarang")
-    fun getLatestNews(): retrofit2.Call<ResponseListBarang>
 
     @GET("webservice/listjenis")
     suspend fun getListJenis() : ResponseListJenis
@@ -22,4 +19,15 @@ interface ApiServices {
 
     @POST("webservice/login")
     suspend fun getLogin(@Body inputLogin: InputLogin) : ResponseLogin
+
+    @Multipart
+    @POST("webservice/tambah-barang")
+    suspend fun tambahBarang(
+        @Part("kode_barang") kodeBarang: RequestBody,
+        @Part("nama_barang") namaBarang: RequestBody?,
+        @Part("jenis_barang") jenisBarang: RequestBody?,
+        @Part("harga_beli") hargaBeli: RequestBody?,
+        @Part("tanggal_masuk") tanggalMasuk: RequestBody?,
+        @Part gambar: MultipartBody.Part
+    ): ResponseGeneral
 }
