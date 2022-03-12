1a9e2553f1cab9e7f7e3994fc1f06b1235d6b8f9
Miftakhul1412 <mifta.ervy@gmail.com>
2022-03-13 03:18:08 +0700

    ADAPTER

--- app/src/main/java/com/miftavy/android/inventory/adapter/AdapterSupplier.kt
+++ app/src/main/java/com/miftavy/android/inventory/adapter/AdapterSupplier.kt
@@ -2,11 +2,60 @@ package com.miftavy.android.inventory.adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.miftavy.android.inventory.R
+import com.miftavy.android.inventory.databinding.ItemAdapterStokBinding
+import com.miftavy.android.inventory.databinding.ItemAdapterSupplierBinding
+import com.miftavy.android.inventory.model.DataStokItem
+import com.miftavy.android.inventory.model.DataSupplierItem
 
-class AdapterSupplier : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_adapter_supplier)
+class AdapterSupplier(private val clickItem: (DataSupplierItem?) -> Unit) : RecyclerView.Adapter<AdapterSupplier.ViewHolder>() {
+
+    //memanggil class utama yaitu class adapterbarang
+
+    inner class ViewHolder(private val binding: ItemAdapterSupplierBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindItem(supplier: DataSupplierItem?) {
+            binding.idSupplier.text = supplier?.idSupplier.toString()
+            binding.namaSupplier.text = supplier?.namaSupplier.toString()
+            binding.alamat.text = supplier?.alamat.toString()
+            binding.kontak.text = supplier?.kontak.toString()
+//            binding.namaBarang.text = barang?.namaBarang
+
+            //inti klik untuk masing-masing baris ke recyclerview nya
+            binding.root.setOnClickListener {
+                clickItem(supplier)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            //memanggil layout itemadapterbarang
+            ItemAdapterSupplierBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(listSupplier?.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return listSupplier.size
+    }
+    //mutablelist adalah array
+    private var listSupplier = mutableListOf<DataSupplierItem?>()
+
+    fun  addItem(list: List<DataSupplierItem?>, clearAll: Boolean = false){
+        if (clearAll)
+            listSupplier = mutableListOf()
+
+        listSupplier.addAll(list)
+
+        //notifyItemInserted(listBarang.size)
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/miftavy/android/inventory/adapter/AdapterUser.kt
+++ app/src/main/java/com/miftavy/android/inventory/adapter/AdapterUser.kt
@@ -2,11 +2,61 @@ package com.miftavy.android.inventory.adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.miftavy.android.inventory.R
+import com.miftavy.android.inventory.databinding.ItemAdapterSupplierBinding
+import com.miftavy.android.inventory.databinding.ItemAdapterUserBinding
+import com.miftavy.android.inventory.model.DataSupplierItem
+import com.miftavy.android.inventory.model.DataUserItem
 
-class AdapterUser : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_adapter_user)
+class AdapterUser(private val clickItem: (DataUserItem?) -> Unit) : RecyclerView.Adapter<AdapterUser.ViewHolder>() {
+
+    //memanggil class utama yaitu class adapterbarang
+
+    inner class ViewHolder(private val binding: ItemAdapterUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindItem(user: DataUserItem?) {
+            binding.id.text = user?.id.toString()
+            binding.name.text = user?.name.toString()
+            binding.email.text = user?.email.toString()
+            binding.jabatan.text = user?.jabatan.toString()
+            binding.level.text = user?.level.toString()
+//            binding.namaBarang.text = barang?.namaBarang
+
+            //inti klik untuk masing-masing baris ke recyclerview nya
+            binding.root.setOnClickListener {
+                clickItem(user)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            //memanggil layout itemadapterbarang
+            ItemAdapterUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(listUser?.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return listUser.size
+    }
+    //mutablelist adalah array
+    private var listUser = mutableListOf<DataUserItem?>()
+
+    fun  addItem(list: List<DataUserItem?>, clearAll: Boolean = false){
+        if (clearAll)
+            listUser = mutableListOf()
+
+        listUser.addAll(list)
+
+        //notifyItemInserted(listBarang.size)
+        notifyDataSetChanged()
     }
 }
