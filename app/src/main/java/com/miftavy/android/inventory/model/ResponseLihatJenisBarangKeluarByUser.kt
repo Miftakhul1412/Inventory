13987a693db66d495e3f9770c8b9d8d596bc533c
Nur Khofifah <fifah250801@gmail.com>
2022-03-23 09:46:57 +0700

    response lihst jenis dn history

---
+++ app/src/main/java/com/miftavy/android/inventory/model/ResponseLihatJenisBarangKeluarByUser.kt
@@ -0,0 +1,39 @@
+package com.miftavy.android.inventory.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseLihatJenisBarangKeluarByUser(
+
+	@field:SerializedName("data_barang_keluar_by_user")
+	val dataBarangKeluarByUser: List<DataBarangKeluarByUserItem?>? = null
+)
+
+data class DataBarangKeluarByUserItem(
+
+	@field:SerializedName("keterangan")
+	val keterangan: String? = null,
+
+	@field:SerializedName("jumlah")
+	val jumlah: Int? = null,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("kode_barang_keluar")
+	val kodeBarangKeluar: Int? = null,
+
+	@field:SerializedName("pengguna")
+	val pengguna: String? = null,
+
+	@field:SerializedName("kode_barang")
+	val kodeBarang: String? = null,
+
+	@field:SerializedName("tanggal_keluar")
+	val tanggalKeluar: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@field:SerializedName("jenis_barang")
+	val jenisBarang: String? = null
+)
